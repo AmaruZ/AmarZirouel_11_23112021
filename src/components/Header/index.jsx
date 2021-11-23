62fec8313ea4c9343f71430ccc24a511aa55b503
Amar <a.zirouel@gmail.com>
2021-11-23 15:31:12 +0100

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/logo.svg'
+
+function Header(){
+    return(
+        <header>
+            <img src={logo} alt="Kasa" />
+        </header>
+    )
+}
+
+export default Header
